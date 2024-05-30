3626026d194b598d13d3f22b83ef56dc4b5d8c23
Azriel14 <awesomezoroark@gmail.com>
2024-05-31 09:34:30 +1000

    Created an animation for the main menu

--- Scripts/MenuAnimation.gd
+++ Scripts/MenuAnimation.gd
@@ -1,10 +1,34 @@
 extends CharacterBody2D
 
 var rng = RandomNumberGenerator.new()
+var playerTouched = false
 
-func _physics_process(delta):
-	var randomNumber = rng.randf_range(128, 960)
-	velocity.x = randomNumber
-	position.y = randomNumber
+@onready var randomNumberVelocityMinus = rng.randf_range(100, 0)
+
+func _physics_process(_delta):
+
+	if playerTouched:
+		$"../PlayerMenu".velocity.x = 0
+	else:
+		velocity.x = Singleton.randomNumberVelocity
+		position.y = Singleton.randomNumberY
+		if $"../EnemyMenu":
+			$"../EnemyMenu".velocity.x -= randomNumberVelocityMinus
+			if $"../EnemyMenu".velocity.x <= 200:
+				$"../EnemyMenu".velocity.x = 400
 
 	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		playerTouched = true
+		$"../PlayerMenu".position.x = RandomNumberGenerator.new().randf_range(-50, -150)
+	if body.is_in_group("Enemy"):
+		playerTouched = false
+		$"../EnemyMenu".position.x = RandomNumberGenerator.new().randf_range(-200, -800)
+		get_tree().paused = true
+		await get_tree().create_timer(RandomNumberGenerator.new().randf_range(1, 5)).timeout
+		get_tree().paused = false
+		Singleton._change_velocity()
+		Singleton._change_y()
+
--- Scripts/Singleton.gd
+++ Scripts/Singleton.gd
@@ -1,6 +1,16 @@
 extends Node
 
 var stage = 1
+var rng = RandomNumberGenerator.new()
+
+@onready var randomNumberY = rng.randf_range(128, 960)
+@onready var randomNumberVelocity = rng.randf_range(400, 900)
 
 func _increment_stage():
 	stage += 1
+
+func _change_y():
+	randomNumberY = rng.randf_range(128, 960)
+
+func _change_velocity():
+	randomNumberVelocity = rng.randf_range(400, 900)
